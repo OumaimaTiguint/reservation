66e70409ea90d7b3924cff3deb037b5d992b24f5
OumaimaTiguint <oumaimatiguint@gmail.com>
2020-11-11 13:02:50 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 import './App.css';
+import moment from 'moment';
+import Time from './time';
 
 function App() {
+  const [dateState, setDateState] = useState(new Date())
+  const changeDate = (e) => {
+    setDateState(e)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>予約日時をお選びください</h2>
+      <Calendar 
+        className="cal"
+        value={dateState}
+        onChange={changeDate}
+      />
+      <p>現在選択されている日付は <b>{moment(dateState).format('MMMM Do YYYY')}</b></p>
+      <Time />
+      <p>※予約サイトからはご利用2時間前までご予約いただけます。</p>
     </div>
   );
 }
